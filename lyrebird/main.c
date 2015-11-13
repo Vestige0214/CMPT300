f651788a7be3fe420b8ec83055f4bb38f35ba4e3
patterson <cpw4@sfu.ca>
2015-11-12 16:36:35 -0800

    FCFS is now in improper mode

--- lyrebird/main.c
+++ lyrebird/main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
 	char out_string[MAX_PATH] = { 0 };
 
 	int fd[num_cores - 1][2]; //parent write to child
-	int pd[2]; //child write to parent as a lineup
+	int pd[num_cores-1][2]; //child write to parent as a lineup
 
 	int string_status = 0;
 
@@ -25,8 +25,8 @@ int main(int argc, char **argv) {
 	int a;
 	/*Initializing child processes: each will have 2 pipes*/
 	for (a = 0; a < num_cores - 1; a++) {
+		pipe(pd[a]);
 		pipe(fd[a]);
-		pipe(pd);
 		pid = fork();
 		if (pid < 0) {
 			printf("[%s] Child creation failed. Process ID #%d\n", getTime(),
@@ -35,11 +35,11 @@ int main(int argc, char **argv) {
 			printf("[%s] Child ID #%d created\n", getTime(), getpid());
 			int buf = a;
 			close(fd[a][1]); //closing write channel
-			close(pd[0]);
+			close(pd[a][0]);
 			char config_line[MAX_LENGTH + 1] = { 0 };
 			int true = 1;
 			while (true) {
-				int write_r = write(pd[1], &buf, sizeof(buf));
+				int write_r = write(pd[a][1], &buf, sizeof(buf));
 				int read_r = read(fd[a][0], config_line, sizeof(config_line));
 				if (strcmp(config_line, end_token) == 0) {
 					break;
@@ -60,17 +60,12 @@ int main(int argc, char **argv) {
 	int mode;
 	int counter = 0;
 	close(fd[a][0]);
-	close(pd[1]);
 	FILE* config_file = myFileOpen(argv[1], "r");
 	char config_line[MAX_LENGTH + 1] = { 0 };
 	while (feof(config_file) == 0) {
 		if (feof(config_file) == 1) {
 			break;
 		}
-		/**
-		 * this is where we found the mode, so from here on we will read each individual config until we find another mode
-		 * so what we should do is continuously check if it is a mode string and if not then it is an address
-		 */
 		myGetString(config_line, config_file, 0);
 		string_status = getStatus(config_line);
 		if (string_status != 0) {
@@ -80,7 +75,9 @@ int main(int argc, char **argv) {
 			if (strcmp(config_line, "\n") == 0) {
 				break;
 			}
+			int buf;
 			int choose_child = counter % (num_cores - 1);
+			read(pd[choose_child][0], &buf, sizeof(buf));
 			write(fd[choose_child][1], config_line, sizeof(config_line));
 			counter++;
 		}
@@ -89,17 +86,23 @@ int main(int argc, char **argv) {
 				break;
 			}
 			int buf;
-			close(pd[1]);
-//			for (a = 0; a < num_cores - 1; a++) {
-//				int read_r = read(pd[0], &buf[a], sizeof(buf[a]));
-//				if (read_r == -1) {
-//					buf[a] = 100;
-//					printf("Error on read is: %s\n", strerror(errno));
-//				}
-//				printf("What is buf[%d]: %d\n", a, buf[a]);
-//			}
-			read(pd[0], buf, sizeof(buf));
-			write(fd[buf][1], config_line, sizeof(config_line));
+			int x;
+			int read_r = 0;
+			for (x = 0; x < num_cores - 1; x++){
+				fcntl(pd[x][0], F_SETFL, O_NONBLOCK);
+			}
+			int true = 1;
+			while (true) {
+				x = x % (num_cores-1);
+				close(pd[x][1]);
+				if ((read_r = read(pd[x][0], &buf, sizeof(buf))) != -1) {
+					write(fd[buf][1], config_line, sizeof(config_line));
+					break;
+				}else{
+					x++;
+				}
+			}
+
 		}
 	}
 	int z;
@@ -129,8 +132,9 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
-//	int c;
-//	for (c = 0; c < num_cores - 1; c++) {
-//		close(fd[c]);
-//	}
+	int c;
+	for (c = 0; c < num_cores - 1; c++) {
+		close(fd[c]);
+		close(pd[c]);
+	}
 }
